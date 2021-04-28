885ab67af1f3220c8178263a9031fd9c215f632a
Ryan McCutcheon <ryanmccutcheon@Ryans-MacBook-Air.local>
2021-04-28 13:18:38 -0500

    Home Screen UI Sidebar

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+import { Container } from 'react-bootstrap'
+import Header from './components/header/Header'
+import Sidebar from './components/sidebar/Sidebar'
+import HomeScreen from './screens/homeScreen/HomeScreen'
+import './_app.scss'
+
+const App = () => {
+    const [sidebar, toggleSidebar] = useState(false)
+    const handleToggleSidebar = () => toggleSidebar(value => !value)
+
+    return (
+        <>
+            <Header handleToggleSidebar={handleToggleSidebar} />
+            <div className="app__container border border-info">
+                <Sidebar
+                    sidebar={sidebar}
+                    handleToggleSidebar={handleToggleSidebar}
+                />
+                <Container className="app__main border border-warning" fluid>
+                    <HomeScreen />
+                </Container>
+            </div>
+        </>
+    )
+}
+
+export default App
---
+++ src/components/categoriesBar/CategoriesBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './_categories.scss'
+
+const CategoriesBar = () => {
+    return (
+        <div>
+            Bar
+        </div>
+    )
+}
+
+export default CategoriesBar
