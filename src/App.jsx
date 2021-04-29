30e609586a24435dc424ad2e18f0a13ecd66a5b4
Ryan McCutcheon <ryanmccutcheon@Ryans-MacBook-Air.local>
2021-04-29 12:08:31 -0500

    React Router Setup

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,49 @@ import Sidebar from './components/sidebar/Sidebar'
 import HomeScreen from './screens/homeScreen/HomeScreen'
 import LoginScreen from './screens/homeScreen/loginScreen/LoginScreen'
 import './_app.scss'
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom'
 
-const App = () => {
+const Layout = ({ children }) => {
     const [sidebar, toggleSidebar] = useState(false)
     const handleToggleSidebar = () => toggleSidebar(value => !value)
-
     return (
-        // <>
-        //     <Header handleToggleSidebar={handleToggleSidebar} />
-        //     <div className="app__container">
-        //         <Sidebar
-        //             sidebar={sidebar}
-        //             handleToggleSidebar={handleToggleSidebar}
-        //         />
-        //         <Container className="app__main" fluid>
-        //             <HomeScreen />
-        //         </Container>
-        //     </div>
-        // </>
-        <LoginScreen />
+        <>
+            <Header handleToggleSidebar={handleToggleSidebar} />
+            <div className="app__container">
+                <Sidebar
+                    sidebar={sidebar}
+                    handleToggleSidebar={handleToggleSidebar}
+                />
+                <Container className="app__main" fluid>
+                    {children}
+                </Container>
+            </div>
+        </>
+    )
+}
+
+const App = () => {
+    return (
+        <Router>
+            <Switch>
+                <Route path="/" exact>
+                    <Layout>
+                        <HomeScreen />
+                    </Layout>
+                </Route>
+                <Route path="/auth">
+                    <LoginScreen />
+                </Route>
+                <Route path="/search">
+                    <Layout>
+                        <h1>Search Results</h1>
+                    </Layout>
+                </Route>
+                <Route>
+                    <Redirect t0="/" />
+                </Route>
+            </Switch>
+        </Router>
     )
 }
 
---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBJ7bULzeXOlMHNdMjcIVoY1LeYMoUhUUc",
+    authDomain: "clone-8bf44.firebaseapp.com",
+    projectId: "clone-8bf44",
+    storageBucket: "clone-8bf44.appspot.com",
+    messagingSenderId: "27017823427",
+    appId: "1:27017823427:web:d59337be9da19f5d0ace76",
+    measurementId: "G-60NPQMZX59"
+}
+
+firebase.initializeApp(firebaseConfig)
